b4cd8fc560d802ac662afc6caecd36a836ad1b50
flyingaround1 <47225988+flyingaround1@users.noreply.github.com>
2019-07-05 03:00:13 -0400

    movies

---
+++ moviething/src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './App.css';
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor(props)
+  {
+    super(props)
+
+    this.state = {
+      movies: []
+    }
+
+    var divStyle = {
+      color: 'white',
+      backgroundImage: 'url(' + imgUrl + ')',
+    };
+
+    // hey, go grab all of the stuff from that webpage
+    axios.get("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=b6fbc7f3f313bd395902af464ef47262")
+      .then((response) => {
+        // handle success
+        this.setState({movies: response.data.results}); // this is our movies
+      })
+    console.log('right after that axios get stuff!')
+    // then put it into this.state.movies
+  }
+
+  
+
+  render() {
+    console.log('Render function!', this.state.movies);
+    return (
+      <div className="App">
+        {this.state.movies.map((movie) => {
+          return (
+          <div>
+            <h1 style={{ color: 'red'}}>
+            {movie.title}</h1>
+            <img src={'https://image.tmdb.org/t/p/w500' + movie.poster_path} alt="Movie Poster" />
+            <p style={{ color: 'white',  }} >{movie.overview}</p>
+          </div>
+          )
+        })}
+      </div>
+    );
+  }
+  
+}
+
+export default App;
---
+++ moviething/src/movie.js
@@ -0,0 +1,26 @@
+    
+import React, { Component } from 'react';
+import './App.css';
+
+class Movie extends Component {
+    render() {
+        const movie=this.props.individualMovie;
+    return (
+        <div className="App">
+            <div className="backdrop" style={{backgroundImage: "url(https://image.tmdb.org/t/p/original" + this.props.individualMovie.backdrop_path+")"}}>
+                <div className="movieBackground">
+                    <div className="Movies">
+                        <h2>{movie.title}</h2>
+                        <h3>Budget: ${movie.budget}</h3>
+                        <h3>Release Date: {movie.release_date}</h3>
+                        <p>Overview: {movie.overview}</p>
+                        <p>Rating: {movie.vote_average}</p>
+                    </div>
+                    <button onClick={this.props.goBack}>Back</button>
+                </div>
+            </div>
+        </div>
+        );
+        }
+        }
+    export default Movie;
